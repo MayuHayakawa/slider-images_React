568cc7dd8641254d4af007c8607b6e9ccc4de58b
Mayu <mayu.i.19930327@gmail.com>
2023-03-12 13:47:57 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
+import { useRef, useEffect, useState } from 'react';
+import images from './images';
 
 function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    // scrollWidth: the total width including stuff that is only visible if you scroll
+    console.log(carousel.current.scrollWidth); 
+    // offsetWidth: the space occupied by the element, including padding and borders
+    console.log(carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <motion.div 
+        ref={carousel} 
+        whileTap={{cursor: "grabbing"}}
+        className='carousel'
+      >
+        <motion.div 
+          drag="x" 
+          dragConstraints={{ right: 0, left: -width }} 
+          className='inner-carousel'
         >
-          Learn React
-        </a>
-      </header>
+          {images.map((image) => {
+            return(
+              <motion.div className='item'>
+                <img src={image} alt="" />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
